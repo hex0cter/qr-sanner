a1f0e0b293b65bb26c77f314b5761ff1e72b3507
Daniel Han <daniel.han@scrive.com>
2021-03-27 15:52:08 +0100

    File restructure

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:qr_scanner/ui/camera_scanner_screen.dart';
-import 'package:qr_scanner/ui/photo_scanner_screen.dart';
+import 'package:qr_scanner/screens/camera_scanner_screen.dart';
+import 'package:qr_scanner/screens/photo_scanner_screen.dart';
 
 void main() => runApp(MaterialApp(initialRoute: '/camera', routes: {
       "/camera": (context) => CameraScannerScreen(),
